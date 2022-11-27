8e991135716e3d5f10cad46ccd789b5e97560e09
unknown <henrytesting8@gmail.com>
2022-11-27 16:30:52 +0700

    passing props

--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -1,12 +1,17 @@
 import React from "react";
+import './ExpensesItem.css';
 
-function ExpensesItem() {
+function ExpensesItem(props) {
+  // const expenseDate = new Date(2021,2,28)
+  // const expenseTitle = 'Car Insurance'
+  // const expenseAmount = '$100'
+
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
