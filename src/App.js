69e11c7072eae2e57e378bf6e29fa13298b55c61
Henry.Yeo <Henry.Yeo@easecoxgroup.com>
2022-12-23 12:07:30 +0700

    using stateful list l-64

--- src/App.js
+++ src/App.js
@@ -1,52 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 import ExpensesFilter from "./components/Expenses/ExpensesFilter";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
   // return React.createElement(
   //   'div',
   //   {},
   //   React.createElement('h2',{style: {textAlign: "center"}},"Schedule"),
   //   React.createElement(Expenses,{items: expenses})
   // )
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const addExpenseHandler = expense =>{
+  const addExpenseHandler = (addExpense) => {
+    // const saveExpense = addExpense
+    // console.log(addExpense)
+    setExpenses((prevExpenses) => {
+      return [addExpense, ...expenses];
+    });
 
-    const saveExpense = expense
-    console.log(saveExpense)
-  }
+  };
 
   return (
     <div>
-      <h2 style={{textAlign:"center"}}>Schedule</h2>
-      <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expenses items={expenses}/>
+      <h2 style={{ textAlign: "center" }}>Schedule</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
