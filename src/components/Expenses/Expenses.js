88de6ae5f82d0cf057020ccb4b35d9eb1bfa4485
Henry.Yeo <Henry.Yeo@easecoxgroup.com>
2022-12-23 17:58:03 +0700

    turnary condition

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,14 +10,27 @@ function Expenses(props) {
   const [selectedYear, setSelectedYear] = useState("2020");
 
   const filteredYear = (selectedYear) => {
-    //console.log(selectedYear)
     setSelectedYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense =>{
-    return expense.date.getFullYear().toString()=== selectedYear;
-  })
-  //console.log(filteredExpenses)
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  });
+
+  // selain turnary conditon bisa digunakan cara ini
+  //--------------------------------------------
+  let expensesList = <p> No Expenses</p>;
+
+  if (filteredExpenses.length > 0)
+    expensesList = filteredExpenses.map((expense) => (
+      <ExpensesItem
+        key={expense.id}
+        date={expense.date}
+        title={expense.title}
+        amount={expense.amount}
+      />
+    ));
+  //--------------------------------------------
 
   return (
     <div>
@@ -27,44 +40,37 @@ function Expenses(props) {
           onChangeFilteredYear={filteredYear}
         />
 
-        {/* {props.items.map((expense) => (
-          <ExpensesItem
-            key={expense.id}
-            date={expense.date}
-            title={expense.title}
-            amount={expense.amount}
-          />
-        ))} */}
+        {/* {
+          // turnary codition ...............
+          filteredExpenses.length == 0 ? (
+            <p>No Expenses </p>
+          ) : (
+            filteredExpenses.map((expense) => (
+              <ExpensesItem
+                key={expense.id}
+                date={expense.date}
+                title={expense.title}
+                amount={expense.amount}
+              />
+            ))
+          )
+        } */}
 
-        {filteredExpenses.map((expense) => (
-          <ExpensesItem
-            key={expense.id}
-            date={expense.date}
-            title={expense.title}
-            amount={expense.amount}
-          />
-        ))}
-
-        {/* <ExpensesItem
-          date={items[0].date}
-          title={items[0].title}
-          amount={items[0].amount}
-        />
-        <ExpensesItem
-          date={items[1].date}
-          title={items[1].title}
-          amount={items[1].amount}
-        />
-        <ExpensesItem
-          date={items[2].date}
-          title={items[2].title}
-          amount={items[2].amount}
-        />
-        <ExpensesItem
-          date={items[3].date}
-          title={items[3].title}
-          amount={items[3].amount}
-        /> */}
+         
+       
+          {/* {filteredExpenses.length == 0 && <p>No Expenses </p>}
+          {filteredExpenses.length >=0 &&
+            filteredExpenses.map((expense) => (
+              <ExpensesItem
+                key={expense.id}
+                date={expense.date}
+                title={expense.title}
+                amount={expense.amount}
+              />
+            ))
+          } cara lain.......................*/}
+        
+          {expensesList}
       </Card>
     </div>
   );
